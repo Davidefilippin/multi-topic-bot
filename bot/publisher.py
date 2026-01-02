4a0cf056737ec7cd11dcb530c2c1a932533aafe9
davide <104149719+Davidefilippin@users.noreply.github.com>
2026-01-02 17:53:17 +0100

    Update publisher.py

--- bot/publisher.py
+++ bot/publisher.py
@@ -1,1 +1,11 @@
+import os
+from telegram import Bot
 
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+bot = Bot(token=TOKEN)
+
+def send_message(title, link):
+    message = f"📰 {title}\n{link}"
+    bot.send_message(chat_id=CHAT_ID, text=message)
