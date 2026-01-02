4fe11d8c88151382e9f4632ca165770a6cc7806a
davide <104149719+Davidefilippin@users.noreply.github.com>
2026-01-02 17:52:42 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,10 @@
+from fetcher import fetch_articles
+from publisher import send_message
 
+def main():
+    articles = fetch_articles()
+    for article in articles:
+        send_message(article["title"], article["link"])
+
+if __name__ == "__main__":
+    main()
