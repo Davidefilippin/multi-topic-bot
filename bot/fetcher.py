51127add7d6a3fd13095fa46d47a7022a12bacf7
davide <104149719+Davidefilippin@users.noreply.github.com>
2026-01-02 17:52:59 +0100

    Update fetcher.py

--- bot/fetcher.py
+++ bot/fetcher.py
@@ -1,1 +1,16 @@
+import feedparser
 
+FEEDS = [
+    "https://www.ansa.it/sito/ansait_rss.xml"
+]
+
+def fetch_articles():
+    articles = []
+    for url in FEEDS:
+        feed = feedparser.parse(url)
+        for entry in feed.entries[:3]:
+            articles.append({
+                "title": entry.title,
+                "link": entry.link
+            })
+    return articles
